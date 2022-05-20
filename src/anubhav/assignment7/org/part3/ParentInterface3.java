29fe7a73050f5c22b3d0734425d8c0a737cb105b
Anubhav-Zemosolabs <anubhav.gupta@zemosolabs.com>
2022-05-20 11:17:17 +0530

    done with all the correction in assignments

--- src/anubhav/assignment7/org/part3/ParentInterface3.java
+++ src/anubhav/assignment7/org/part3/ParentInterface3.java
@@ -1,6 +1,6 @@
 package anubhav.assignment7.org.part3;
 
 public interface ParentInterface3 {
-    void method1_ParentInterface3();
-    void method2_ParentInterface3();
+    void method1ParentInterface3();
+    void method2ParentInterface3();
 }
